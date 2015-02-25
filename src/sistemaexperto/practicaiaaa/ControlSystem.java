9211217d4431ba8b00562cec9c34e27cb9be0c1e
alu0100764666 <alu0100764666@ull.edu.es>
2015-02-25 15:54:06 +0000

    Terminado sistema de control

--- src/sistemaexperto/practicaiaaa/ControlSystem.java
+++ src/sistemaexperto/practicaiaaa/ControlSystem.java
@@ -9,7 +9,7 @@ public class ControlSystem {
 	private Tank tank;
 	private Valve inValve;
 	private Valve outValve;
-	
+	private final int valveLimit = 100;
 	/**
 	 * @param t Tanque a controlar.
 	 * @param inV Valvula de entrada
@@ -21,4 +21,81 @@ public class ControlSystem {
 		outValve = oV;
 	}
 
+	public void control () {
+		double num = 0;
+		double div = 0;
+		double aux;
+		
+		for (int i = -25; i < valveLimit; i+=5) {
+			 aux = Math.max(Math.max(Math.min(errorNegativo(tank.getError()), valvulaCerrada(i)), Math.min(errorCero(tank.getError()), valvulaCerrada(i))), Math.min(errorPositivo(tank.getError()), valvulaAbierta(i)));
+			 num += aux * i;
+			 div += aux;
+		}
+		inValve.setApertura(num / div);
+	}
+	
+	public void nextSecond() {
+		tank.addWaterToTank(inValve.getWaterFlow());
+		tank.addWaterToTank(-outValve.getWaterFlow());
+	}
+	
+	private double errorNegativo(double error) {
+		double v;
+		
+		if (error <= -10 && error >= -15)
+			v = 1;
+		else if (error <= 0 && error >= -10)
+			v = -error / 10;
+		else 
+			v = 0;
+		return v;
+	}
+	
+	private double errorCero(double error) {
+		double v;
+		
+		if (error <= 0 && error >= 5)
+			v = (5 + error) / 5;
+		else if (error <= 5 && error >= 0)
+			v =  (5 - error) / 5;
+		else 
+			v = 0;
+		return v;
+	}
+	
+	private double errorPositivo(double error) {
+		double v;
+		
+		if (error <= 10 && error >= 0)
+			v = error / 10;
+		else if (error <= 15 && error >= 10)
+			v = 1;
+		else 
+			v = 0;
+		return v;
+	}	
+	
+	private double valvulaAbierta (double apertura) {
+		double v;
+		
+		if (apertura <= 50 && apertura >= 0)
+			v = apertura / 50;
+		else if (apertura >= 50)
+			v = 1;
+		else 
+			v = 0;
+		return v;
+	}
+	
+	private double valvulaCerrada (double apertura) {
+		double v;
+		
+		if (apertura <= 0 && apertura >= -25)
+			v = (25 + apertura) / 25;
+		else if (apertura >= 0 && apertura <= 25)
+			v = (25 - apertura) / 25;
+		else 
+			v = 0;
+		return v;
+	}
 }

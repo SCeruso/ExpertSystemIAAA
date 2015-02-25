3c3f5039a1c01fec51d3d1a447f1a85e5bd2abee
Eldon <alu0100777758@edu.ull.es>
2015-02-25 23:26:58 +0000

    añadido launcher

---
+++ src/sistemaexperto/practicaiaaa/LaunchSimulacion.java
@@ -0,0 +1,26 @@
+package sistemaexperto.practicaiaaa;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class LaunchSimulacion {
+	public static void main (String[] args){
+		Simulacion sdc = new Simulacion(18000);
+		if(args.length>0){
+			System.out.println("el nombre del fichero es:" + args[0]);
+			PrintStream output = openStreamToFile(args[0]);
+			System.setOut(output);
+		}
+		sdc.simular();
+	}
+	static PrintStream openStreamToFile(String  filename){
+		try{
+		return new PrintStream(new FileOutputStream(filename));
+		}catch(IOException e){
+			System.err.println("Error abriendo el fichero");
+			System.exit(-1);
+		}
+		return null;
+	}
+}
